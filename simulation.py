6e4be34a6b601edec44655ecd82ff9c01b6d7b40
kuseler <kimue-28@web.de>
2022-12-21 22:28:37 +0100

    finalized presentation part

--- simulation.py
+++ simulation.py
@@ -4,10 +4,10 @@ import matplotlib.pyplot as plt
 def versuch(wechsel):
     # Tür 1 ist das Auto, Türen 2 und 3 sind Ziegen
     gewaehlte_tuer = random.randint(1, 3) # zufällige Tür mit der Nummer 1-3
-    if wechsel: # Wenn es wechselt, 
-        revealed_door = 3 if gewaehlte_tuer==2 else 2
-        available_doors = [i for i in range(1,4) if i not in (gewaehlte_tuer, revealed_door)]
-        gewaehlte_tuer = random.choice(available_doors)
+    if wechsel: # Wenn man wechselt, dann:
+        geoeffnete_tuer = 3 if gewaehlte_tuer==2 else 2
+        verfuegbare_tueren = [i for i in range(1,4) if i not in (gewaehlte_tuer, geoeffnete_tuer)]
+        gewaehlte_tuer = random.choice(verfuegbare_tueren)
     if gewaehlte_tuer == 1:
         return "Auto"
     else:
@@ -15,9 +15,9 @@ def versuch(wechsel):
 
 
 def main():
-    m = [run_trial(True) for i in range(100000)]
-    o = [run_trial(False) for i in range(100000)]
-    print(f'Siegquote mit Wechsel:  {m.count("Auto")/1000}% \nSiegquote ohne Wechsel: {o.count("Auto")/1000}%')
+    m = [versuch(True) for i in range(100000)]
+    o = [versuch(False) for i in range(100000)]
+    print(f'Autoquote mit Wechsel:  {m.count("Auto")/1000}% \nAutoquote ohne Wechsel: {o.count("Auto")/1000}%')
     plot_res(m,o)
 
 
@@ -27,7 +27,7 @@ def plot_res(p1, p2):
     erg2 = [p2.count("Auto")/1000,p2.count("Ziege")/1000]    
     plt.bar(bes,erg1, color='r')
     plt.bar(bes, erg2, bottom=erg1, color='b')
-    plt.legend(["Siege", "Niederlagen"])
+    plt.legend(["Autos", "Ziegen"])
     plt.show()
 
 
